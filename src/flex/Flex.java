c91b9ede3cfdfe0a53f2e4322079e35c8c28a258
danishali98 <l176317@lhr.nu.edu.pk>
2021-06-06 20:10:21 +0500

    Final Version

---
+++ src/flex/Flex.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flex;
+
+/**
+ *
+ * @author Danish Ali
+ */
+public class Flex {
+
+    
+    public static void main(String[] args) {
+       dbconnectivity db = new dbconnectivity();
+       db.test();
+        
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new Login().setVisible(true);
+//            }
+//        });
+    }
+    
+}
